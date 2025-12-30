de9022e2c767da78c3b365257e7b5dbfcf3e529f
Alan-MOK <alan_gen.l@hotmail.com>
2025-12-30 15:38:01 +0800

    Initial Commit

---
+++ MulTab/MulTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MulTabApp.swift
+//  MulTab
+//
+//  Created by Alan Mok on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct MulTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MulTabTests/MulTabTests.swift
@@ -0,0 +1,17 @@
+//
+//  MulTabTests.swift
+//  MulTabTests
+//
+//  Created by Alan Mok on 2025/12/30.
+//
+
+import Testing
+@testable import MulTab
+
+struct MulTabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
