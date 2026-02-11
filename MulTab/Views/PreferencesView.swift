01ddca303eb5cc4d7cf5e2afc6896d230d84f06b
Alan Mok <alan_gen.l@hotmail.com>
2026-02-11 16:22:41 +0800

    rename version

--- MulTab/Views/PreferencesView.swift
+++ MulTab/Views/PreferencesView.swift
@@ -93,7 +93,7 @@ struct PreferencesView: View {
             HStack {
                 Image(systemName: "info.circle")
                     .foregroundColor(.secondary)
-                Text("Version 2.11.0")
+                Text("Version 2.11.1")
                     .font(.caption)
                     .foregroundColor(.secondary)
                 
@@ -106,7 +106,7 @@ struct PreferencesView: View {
             }
         }
         .padding(20)
-        .frame(width: 400, height: 420)
+        .frame(width: 400, height: 500)
     }
 }
 
@@ -116,7 +116,7 @@ final class PreferencesWindowController: NSWindowController {
     
     private init() {
         let window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 400, height: 420),
+            contentRect: NSRect(x: 0, y: 0, width: 400, height: 500),
             styleMask: [.titled, .closable],
             backing: .buffered,
             defer: false
